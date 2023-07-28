dc8c7daba8c69e7a7c9b648b3784ca568d64c686
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-07-28 16:26:22 -0300

    add prank code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
-#include <Arduino.h>
+// This DigiSpark script opens up Rick Astley's - Never Gonna Give You Up and also a fake Windows update screen and then maximizes it using F11
+#include "Arduino.h"
+#include "DigiKeyboard.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  // empty
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(5000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("http://fakeupdate.net/win10ue");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_F11);
+  for (;;)
+  { /*empty*/
+  }
 }
